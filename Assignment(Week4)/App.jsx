b4525d0022df04c4f4f11af2a2b192c41626207d
whitepanda2002 <123859064+whitepanda2002@users.noreply.github.com>
2025-04-09 20:33:57 +0530

    Create App.jsx

---
+++ Assignment(Week4)/App.jsx
@@ -0,0 +1,17 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import ProductList from "./components/ProductList";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <ProductList />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
