7d4fc4a54f6e5b12410dfbb9004ea3228d6dd8bd
whitepanda2002 <123859064+whitepanda2002@users.noreply.github.com>
2025-04-09 20:32:25 +0530

    Create Footer.jsx

---
+++ Assignment(Week4)/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => (
+  <footer className="bg-gray-200 py-4 text-center">
+    <p>Follow us on social media</p>
+    <div className="flex justify-center gap-4 mt-2">
+      <a href="#">Facebook</a>
+      <a href="#">Instagram</a>
+      <a href="#">Twitter</a>
+    </div>
+  </footer>
+);
+export default Footer;
